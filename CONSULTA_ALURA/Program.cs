acc50986fa3d126538c129fd6b9da87aad5f0a78
Gabriel do Amaral Guedes <gabriel.guedes@ibm.com>
2024-05-30 10:30:45 -0300

    inicio projeto

---
+++ CONSULTA_ALURA/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * ########################################################################################################
+ * AUTOR: GABRIEL GUEDES
+ * 
+ * DATA ÚLTIMA MODIFICAÇÃO: 30/05/2024
+ * NÚMERO DA VERSÃO: 1.0
+ * 
+ * DATA ÚLTIMA REVISÃO: 30/05/2024
+ * NÚMERO DA REVISÃO: REV.001
+ * ########################################################################################################
+ */
+
+//Classe para consulta de cursos na alura
+
+using System;
+
+namespace CONSULTA_ALURA
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Program/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * ########################################################################################################
+ * AUTOR: GABRIEL GUEDES
+ * 
+ * DATA ÚLTIMA MODIFICAÇÃO: 30/05/2024
+ * NÚMERO DA VERSÃO: 1.0
+ * 
+ * DATA ÚLTIMA REVISÃO: 30/05/2024
+ * NÚMERO DA REVISÃO: REV.001
+ * ########################################################################################################
+ */
+
+using System;
+using CONSULTA_ALURA;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
